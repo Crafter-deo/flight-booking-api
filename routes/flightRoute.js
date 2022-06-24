4632db75fe1a22812ec7521956207de8195d4cbf
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-06-24 04:33:24 +0100

    cleaned up file

--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -3,15 +3,16 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/flightController');
 
-router.get('/flights', controller.getFlights)
+router
+    .get('/flights', controller.getFlights)
 
-router.get('/flights/:id', controller.getFlight)
+    .get('/flights/:id', controller.getFlight)
 
-router.post('/flight', controller.addFlight)
+    .post('/flights', controller.addFlight)
 
-router.put('/flights/:id', controller.updateFlight)
+    .put('/flights/:id', controller.updateFlight)
 
-router.delete('/flights/:id', controller.deleteFlight)
+    .delete('/flights/:id', controller.deleteFlight)
 
 
 
