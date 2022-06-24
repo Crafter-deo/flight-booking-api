881f0603ca5fe26eb92d5d5415ce3f50b886dc0c
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-06-24 04:35:04 +0100

    finished CRUD logic for flight booking

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,35 +1,86 @@
-const data = require("../models/flights.json")
-const fs = require(fs)
+const fs = require("fs")
+const { flights } = require("../models/Flight")
+const { v4: uuidv4 } = require("uuid")
+
 
 exports.getFlights = (req, res) => {
-    console.log("example")
-    let file = fs.readFile("../models/flights.json")
-    res.status(200).send(file)
-    // res.send({
-    // title: "flight to canada",
-    // time: "1 pm",
-    // price: 26000,
-    // date: "26-06-2022"
-    // }
-    // )
+    try {
+        res.status(200).json(flights)
+        console.log(`Request made to get all flights`)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
 }
 
+
 exports.getFlight = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
+    try {
+        const flightId = req.params.id
+        const flight = flights.find((flight) => flight.id === flightId)
+        res.status(200).json(flight)
+        console.log(`Request made to get a flight`)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
 }
 
 exports.addFlight = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
+    try {
+        const flight = req.body
+        flight.id = uuidv4()
+        flights.push(flight)
+        res.status(201).json(
+            {
+                message: "Flight added successfully",
+                flight
+            }
+        )
+        console.log(`Request made to get add a flight`)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
 }
 
-exports.updateFlight = (req, res, next) => {
-    console.log("example")
-    res.send("Flight example")
+exports.updateFlight = (req, res) => {
+    try {
+        const flightUpdate = req.body
+        const flightId = req.params.id
+        let flight = flights.find((flight) => flight.id === flightId)
+        flight.title = flightUpdate.title
+        flight.time = flightUpdate.time
+        flight.price = flightUpdate.price
+        flight.date = flightUpdate.date
+        res.status(200).json({
+            message: "Flight updated successfully",
+            flight
+        })
+        console.log(`Request made to update a flight`)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
+
 }
 
 exports.deleteFlight = (req, res, next) => {
-    console.log("example")
-    res.send("Flight example")
+    try {
+        const flightId = req.params.id
+        let flight = flights.find((flight) => flight.id === flightId)
+        delete flights[flights.indexOf(flight)]
+        res.status(200).json({
+            message: "Flight deleted successfully",
+        })
+        console.log(`Request made to delete a flight`)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        })
+    }
 }
